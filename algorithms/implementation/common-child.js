6b0f6c7dbf64166fe957df74d2980dec6b0486ac
melih <mlhclskn@gmail.com>
2020-11-27 21:46:27 +0300

    Create common-child.js

---
+++ algorithms/implementation/common-child.js
@@ -0,0 +1,17 @@
+// Thanks to brank (https://www.hackerrank.com/bryank)
+function commonChild(s1, s2) {
+    let arr = [Array(s2.length + 1).fill(0)];
+    [...s1].forEach((v1, i) => {
+        arr[i + 1] = [0];
+        [...s2].forEach((v2, j) => {
+            arr[i + 1][j + 1] = v1 === v2 ?
+                arr[i][j] + 1 : Math.max(arr[i + 1][j], arr[i][j + 1]);
+        });
+    });
+    return arr[s2.length][s1.length];
+}
+
+const s1 = 'ABCDEF'
+const s2 = 'FBDAMN'
+
+console.log(commonChild(s1, s2))
